01de2c7e2e3887ad43f70918961a4de08f1f6ec7
Diego Patiño <juanpatinoch@gmail.com>
2020-04-22 21:48:24 -0500

    Se hace conexión con firestore para traer los productos

--- app/src/main/java/com/placetopay/commerce/model/repository/ProductsRepository.kt
+++ app/src/main/java/com/placetopay/commerce/model/repository/ProductsRepository.kt
@@ -1,10 +1,15 @@
 package com.placetopay.commerce.model.repository
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import com.google.firebase.firestore.FirebaseFirestore
 import com.placetopay.commerce.model.Products
 
 class ProductsRepository {
 
+    private val tag = "ProductsRepository"
+
+    private var firebaseFirestore: FirebaseFirestore? = null
     private var products = MutableLiveData<List<Products>>()
 
     fun getProducts(): MutableLiveData<List<Products>> {
@@ -12,42 +17,35 @@ class ProductsRepository {
     }
 
     fun callProducts() {
-        var productsList = ArrayList<Products>()
+        try {
+            if (firebaseFirestore == null)
+                firebaseFirestore = FirebaseFirestore.getInstance()
 
-        var productItem = Products()
-        productItem.code = "8sd8sd77f"
-        productItem.description = "Lorem inpsum"
-        productItem.discountText = "-20%"
-        productItem.imageUrl =
-            "https://firebasestorage.googleapis.com/v0/b/lumarket-2.appspot.com/o/products%2Freebook_royal_glide_1.png?alt=media&token=23075b6c-a3ca-4e90-a0f1-b3ba8f58ace0"
-        productItem.name = "Reebook Royal Glide"
-        productItem.price = 199900
+            firebaseFirestore?.collection("products")?.get()
+                ?.addOnSuccessListener { result ->
+                    var productsList = ArrayList<Products>()
+                    for (document in result) {
+                        var product = Products()
+                        product.code = document.id
+                        product.name = document.data["name"].toString()
+                        product.description = document.data["description"].toString()
+                        product.price = document.data["price"].toString().toInt()
+                        product.image = document.data["image"].toString()
+                        product.header = document.data["header"].toString()
+                        product.discount = document.data["discount"].toString()
+                        product.priceText = document.data["price"].toString()
 
-        productsList.add(productItem)
+                        productsList.add(product)
+                    }
+                    products.value = productsList
+                }
+                ?.addOnFailureListener {
+                    Log.e(tag, it.message)
+                }
+        } catch (e: Exception) {
+            Log.e(tag, e.message)
+        }
 
-        var productItem2 = Products()
-        productItem2.code = "87s66sdd"
-        productItem2.description = "Is a smartwatch"
-        productItem2.discountText = null
-        productItem2.imageUrl =
-            "https://firebasestorage.googleapis.com/v0/b/lumarket-2.appspot.com/o/products%2Fxiaomi_amazfit_bip_1.png?alt=media&token=20cebf06-b3be-409f-bc59-b695c7568bc1"
-        productItem2.name = "Amazfit Bip"
-        productItem2.price = 209950
-
-        productsList.add(productItem2)
-
-        var productItem3 = Products()
-        productItem3.code = "kkfk29944mw"
-        productItem3.description = "Delicioso refrigerio"
-        productItem3.discountText = "Promo!"
-        productItem3.imageUrl =
-            "https://firebasestorage.googleapis.com/v0/b/lumarket-2.appspot.com/o/products%2Fdmartin_wrap.jpg?alt=media&token=66ab3eef-0b25-4296-88ab-88a1f04a0d3b"
-        productItem3.name = "Wrap grande"
-        productItem3.price = 7000
-
-        productsList.add(productItem3)
-
-        products.value = productsList
     }
 
 }
